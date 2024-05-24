f113ce90f3e9e8e53739a87290a77a95bd861932
Josh Rime <joshrime1990@icloud.com>
2024-05-23 23:59:55 -0500

    working routes and link

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,61 +1,68 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import * as Yup from "yup";
 
 // 👇 Here are the validation errors you will use with Yup.
 const validationErrors = {
-  fullNameTooShort: 'full name must be at least 3 characters',
-  fullNameTooLong: 'full name must be at most 20 characters',
-  sizeIncorrect: 'size must be S or M or L'
-}
+  fullNameTooShort: "full name must be at least 3 characters",
+  fullNameTooLong: "full name must be at most 20 characters",
+  sizeIncorrect: "size must be S or M or L",
+};
 
 // 👇 Here you will create your schema.
 
 // 👇 This array could help you construct your checkboxes using .map in the JSX.
 const toppings = [
-  { topping_id: '1', text: 'Pepperoni' },
-  { topping_id: '2', text: 'Green Peppers' },
-  { topping_id: '3', text: 'Pineapple' },
-  { topping_id: '4', text: 'Mushrooms' },
-  { topping_id: '5', text: 'Ham' },
-]
+  { topping_id: "1", text: "Pepperoni" },
+  { topping_id: "2", text: "Green Peppers" },
+  { topping_id: "3", text: "Pineapple" },
+  { topping_id: "4", text: "Mushrooms" },
+  { topping_id: "5", text: "Ham" },
+];
 
 export default function Form() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Order Your Pizza</h2>
-      {true && <div className='success'>Thank you for your order!</div>}
-      {true && <div className='failure'>Something went wrong</div>}
+      {true && <div className="success">Thank you for your order!</div>}
+      {true && <div className="failure">Something went wrong</div>}
 
       <div className="input-group">
         <div>
-          <label htmlFor="fullName">Full Name</label><br />
+          <label htmlFor="fullName">Full Name</label>
+          <br />
           <input placeholder="Type full name" id="fullName" type="text" />
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {true && <div className="error">Bad value</div>}
       </div>
 
       <div className="input-group">
         <div>
-          <label htmlFor="size">Size</label><br />
+          <label htmlFor="size">Size</label>
+          <br />
           <select id="size">
             <option value="">----Choose Size----</option>
-            {/* Fill out the missing options */}
+            <option value="S">Small</option>
+            <option value="M">Medium</option>
+            <option value="L">Large</option>
           </select>
         </div>
-        {true && <div className='error'>Bad value</div>}
+        {true && <div className="error">Bad value</div>}
       </div>
 
       <div className="input-group">
         {/* 👇 Maybe you could generate the checkboxes dynamically */}
         <label key="1">
-          <input
-            name="Pepperoni"
-            type="checkbox"
-          />
-          Pepperoni<br />
+          <input name="Pepperoni" type="checkbox" />
+          Pepperoni
+          <br />
         </label>
       </div>
       {/* 👇 Make sure the submit stays disabled until the form validates! */}
       <input type="submit" />
     </form>
-  )
+  );
 }
